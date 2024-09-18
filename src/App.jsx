a0f958eb363b9c587b94198f34b15bb18127b957
Murad <smrtlearner202@gmail.com>
2024-09-18 16:24:03 +0530

    Initail changes

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Navbar} from './Components/Navbar'
+import Hero from './Components/Hero'
+import HeadlineCards from './Components/HeadlineCards'
+import Food from './Components/Food'
+import Category from './Components/Category'
+
+const App = () => {
+  return (
+    <div>
+       <Navbar/>
+       <Hero/>
+       <HeadlineCards/>
+       <Food/>
+       <Category/>
+      </div>
+  )
+}
+
+export default App
